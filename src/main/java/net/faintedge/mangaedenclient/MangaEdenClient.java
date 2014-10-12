0ad3d9c5e4657b72dd9c32aa23560f561e32b0bf
Jiahua Wang <jiahuawang7@gmail.com>
2014-10-11 23:48:40 -0700

    Use OkHttpClient instead of AsyncHttpClient

--- src/main/java/net/faintedge/mangaedenclient/MangaEdenClient.java
+++ src/main/java/net/faintedge/mangaedenclient/MangaEdenClient.java
@@ -2,8 +2,9 @@ package net.faintedge.mangaedenclient;
 
 import com.google.common.base.Optional;
 import com.google.gson.Gson;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.Response;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,12 +21,12 @@ public class MangaEdenClient {
 
   private static final URI DEFAULT_MANGA_EDEN_URI = URI.create("http://www.mangaeden.com");
 
-  private final AsyncHttpClient httpClient;
+  private final OkHttpClient httpClient;
   private final URI baseURI;
 
   public MangaEdenClient(URI baseURI) {
     this.baseURI = baseURI;
-    this.httpClient = new AsyncHttpClient();
+    this.httpClient = new OkHttpClient();
   }
 
   public MangaEdenClient() {
@@ -39,23 +40,29 @@ public class MangaEdenClient {
   public List<Manga> getMangaList(Optional<Integer> page) throws Exception {
     String suffix = page.isPresent() ? "?p=" + page.get() : "";
     URI uri = baseURI.resolve("/api/list/0/" + suffix);
-    Response response = httpClient.prepareGet(uri.toString()).execute().get();
-    String responseBody = response.getResponseBody();
+    Request request = new Request.Builder().get().url(uri.toURL()).build();
+
+    Response response = httpClient.newCall(request).execute();
+    String responseBody = response.body().string();
     MangaListResponse mangaListResponse = GSON.fromJson(responseBody, MangaListResponse.class);
     return mangaListResponse.getManga();
   }
 
   public MangaDetails getMangaDetails(String mangaId) throws Exception {
     URI uri = baseURI.resolve("/api/manga/" + mangaId + "/");
-    Response response = httpClient.prepareGet(uri.toString()).execute().get();
-    String responseBody = response.getResponseBody();
+    Request request = new Request.Builder().get().url(uri.toURL()).build();
+
+    Response response = httpClient.newCall(request).execute();
+    String responseBody = response.body().string();
     return GSON.fromJson(responseBody, MangaDetails.class);
   }
 
   public ChapterDetails getChapterDetails(String chapterId) throws Exception {
     URI uri = baseURI.resolve("/api/chapter/" + chapterId + "/");
-    Response response = httpClient.prepareGet(uri.toString()).execute().get();
-    String responseBody = response.getResponseBody();
+    Request request = new Request.Builder().get().url(uri.toURL()).build();
+
+    Response response = httpClient.newCall(request).execute();
+    String responseBody = response.body().string();
     return GSON.fromJson(responseBody, ChapterDetails.class);
   }
 
